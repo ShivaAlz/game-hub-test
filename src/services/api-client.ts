d1d586780e7d8c139927448f5c6e59c7cef5d5b4
shiva alz <alemzadeh.shiva@gmail.com>
2023-04-29 11:55:02 +0200

    Building game cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.rawg.io/api",
     params:{
-        key: 'f6a9dd9f38244310afed383aa5d148de'
+        key: '612927b43abe401797f051715200f81f'
     }
 })
