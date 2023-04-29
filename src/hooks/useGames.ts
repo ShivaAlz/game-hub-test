29c83da3a3fdc14ffe16603aa64f2a09eefafc34
shiva alz <alemzadeh.shiva@gmail.com>
2023-04-29 12:50:30 +0200

    Display Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform{
+    id:number;
+    name:string;
+    slug:string
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image:string;
+    parent_platforms:{platform:Platform}[] ; //an array of objects where each object has an platform item
   }
 
   interface FetchGameResponse{
