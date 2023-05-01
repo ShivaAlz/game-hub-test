6b7b46bc6d2132c6804cac1aa117cb3949ebd6ec
shiva alz <alemzadeh.shiva@gmail.com>
2023-05-01 18:36:34 +0200

    Create a generic data fetching hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,43 +1,13 @@
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 
-interface Genre{
+import useData from "./useData";
+
+export interface Genre{
 id:number;
 name:string;
 }
 
-interface FetchGenreResponse{
-    count:number;
-    results:Genre[]
-}
 
-const useGenres=()=>{
-    const [genres, setGenres] = useState<Genre[]>([]);
-        const [errors, setErrors] = useState("");
-        const [isLoading,setLoading]=useState(false)
-      
-        useEffect(() => {
-            const controller=new AbortController(); // for cancelation
-
-            setLoading(true)
-          apiClient
-            .get<FetchGenreResponse>("/genres",{signal:controller.signal})
-            .then((res) =>{ setGenres(res.data.results);
-            setLoading(false);
-            })
-            .catch((err) => {
-                if(err instanceof CanceledError) return;
-                setErrors(err.message)});
-                setLoading(false);
-        
-        //cleanup function
-        return ()=>controller.abort();
-        
-        },[]);
-        
-
-      return {genres,errors,isLoading}
-}
+const useGenres=()=>useData<Genre>('/genres')
+    
 
 export default useGenres;
